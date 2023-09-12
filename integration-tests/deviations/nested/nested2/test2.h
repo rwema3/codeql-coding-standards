de15d89aadc74ba70d463ba22356f3f7516fad27
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-09-12 21:21:10 +0200

    Intergration

--- integration-tests/deviations/nested/nested2/test2.h
+++
@@ -1,6 +0,0 @@
-int getZ() { return 5; }
-
-void test2() {
-  int x = 0; // COMPLIANT[DEVIATED]
-  getZ(); // COMPLIANT[DEVIATED]
-}
--- integration-tests/deviations/nested/test.h
+++
@@ -1,6 +0,0 @@
-int getY() { return 5; }
-
-void test() {
-  int x = 0; // COMPLIANT[DEVIATED]
-  getY(); // NON_COMPLIANT
-}
