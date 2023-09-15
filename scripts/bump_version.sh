08a8cc83d31571bec4f3ae8b274f3e9a13b1474a
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-09-15 17:30:02 +0200

    using manual.md

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -15,3 +15,4 @@ find . -name 'qlpack.yml' | grep -v './codeql_modules' | grep -v './scripts' | x
 # update the documentation. 
 
 find docs -name 'user_manual.md' | xargs sed -i "s/code-scanning-cpp-query-pack-.*\.zip\`/code-scanning-cpp-query-pack-${1}.zip\`/"
+find docs -name 'user_manual.md' | xargs sed -i "s/supported_rules_list_.*\.csv\`/supported_rules_list_${1}.csv\`/"
